00cb24c3dc639174ae55c0b43408451baba0fce1
TyupkinV <dendrarius427@gmail.com>
2018-05-31 15:25:51 +0300

    implemented review link to the index

--- ModelViews/ReferenceIndexModelView.cs
+++ ModelViews/ReferenceIndexModelView.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Data;
+using System;
 
 namespace MedicalRefbook2_0.ModelViews
 {
-    class ReferenceIndexModelView
-    {
+    public class ReferenceIndexModelView : INotifyPropertyChanged{
+
+        private List<Button> _usingIndicesList;
+        private List<Button> _dependIndicesList;
+        private List<string> _infoSelectedIndexList;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public List<Button> UsingIndicesList {
+            get {
+                return _usingIndicesList;
+            }
+            set {
+                _usingIndicesList = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public List<Button> DependIndicesList {
+            get {
+                return _dependIndicesList;
+            }
+            set {
+                _dependIndicesList = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public List<string> InfoSelectedIndex {
+            get {
+                return _infoSelectedIndexList;
+            }
+            set {
+                _infoSelectedIndexList = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ReferenceIndexModelView(object nameIndex, DataSet allData) {
+            Tuple<List<string>, List<Button>, List<Button>> infoObject = Models.RefbookModel.InfoSelectIndex(nameIndex, allData);
+            InfoSelectedIndex = infoObject.Item1;
+            UsingIndicesList = infoObject.Item2;
+            DependIndicesList = infoObject.Item3;
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propName = "") {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
     }
 }
--- Views/ReferenceIndexView.xaml.cs
+++ Views/ReferenceIndexView.xaml.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Data;
 
 namespace MedicalRefbook2_0.Views {
-    /// <summary>
-    /// Логика взаимодействия для ReferenceIndexView.xaml
-    /// </summary>
     public partial class ReferenceIndexView : Window {
-        public ReferenceIndexView() {
+
+        public ReferenceIndexView(object nameIndex, DataSet allData) {
             InitializeComponent();
+            DataContext = new ModelViews.ReferenceIndexModelView(nameIndex, allData);
         }
     }
 }
